029ff319221bcfa07196e712e3c59e02de07ca44
Crispin Wellington <retrogradeorbit@gmail.com>
2019-09-21 00:04:40 +0800

    get system tray launching

--- Makefile
+++ Makefile
@@ -3,12 +3,14 @@ GRAALVM = ~/graalvm-ce-19.2.0.1
 all: build/wireguard-switcher
 
 clean:
-	-rm -rf build
+	-rm -rf build target
 
-build/wireguard-switcher:
-	-mkdir build
+target/uberjar/wireguard-switcher-0.1.0-SNAPSHOT-standalone.jar:
 	lein clean
 	lein uberjar
+
+build/wireguard-switcher: target/uberjar/wireguard-switcher-0.1.0-SNAPSHOT-standalone.jar
+	-mkdir build
 	$(GRAALVM)/bin/native-image \
 		-jar target/uberjar/wireguard-switcher-0.1.0-SNAPSHOT-standalone.jar \
 		-H:Name=build/wireguard-switcher \
@@ -16,9 +18,16 @@ build/wireguard-switcher:
 		-J-Dclojure.spec.skip-macros=true \
 		-J-Dclojure.compiler.direct-linking=true \
 		-H:ReflectionConfigurationFiles=graal-configs/reflection.json \
-		--initialize-at-build-time  \
+		--initialize-at-build-time \
+		--initialize-at-run-time=dorkbox.util.jna,sun.java2d.opengl,sun.awt.X11.XToolkit,java.awt.Toolkit \
 		-H:Log=registerResource: \
 		--verbose \
+		--allow-incomplete-classpath \
 		--no-fallback \
 		--no-server \
-		"-J-Xmx3g"
+		"-J-Xmx3g" \
+		-H:+TraceClassInitialization -H:+PrintClassInitialization --debug-attach
+
+analyse:
+	$(GRAALVM)/bin/java -agentlib:native-image-agent=config-output-dir=config-dir \
+		-jar target/uberjar/wireguard-switcher-0.1.0-SNAPSHOT-standalone.jar
--- src/wireguard_switcher/core.clj
+++ src/wireguard_switcher/core.clj
@@ -1,7 +1,80 @@
 (ns wireguard-switcher.core
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]
+            [clojure.tools.cli :refer [parse-opts]])
+  (:import [dorkbox.systemTray SystemTray MenuItem Separator Checkbox]
+           [java.awt.event ActionListener])
   (:gen-class))
+
+(def version "0.1")
+
+(def cli-options
+  [
+   ["-h" "--help" "Print the command line help"]
+   ["-v" "--version" "Print the version string and exit"]])
+
+(def resources
+  {:dark (io/resource "lock.png")
+   :red (io/resource "lock-red.png")
+   :green (io/resource "lock-green.png")
+   :light (io/resource "lock-light-grey.png")})
+
+(defmacro add-menu-item [menu label & body]
+  `(let [item# (MenuItem. ~label
+                    (proxy [ActionListener] []
+                      (actionPerformed [~'_]
+                        ~@body)))]
+     (.add ~menu item#)
+     item#))
+
+(defmacro add-menu-checkbox [menu label & body]
+  `(let [item# (Checkbox. ~label
+                    (proxy [ActionListener] []
+                      (actionPerformed [~'_]
+                        ~@body)))]
+     (.add ~menu item#)
+     item#))
+
+(defmacro add-menu-separator [menu]
+  `(.add ~menu (Separator.)))
+
+(defn setup-switcher [items]
+  (when-let [tray (SystemTray/get)]
+    (let [icon (:dark resources)]
+      (doto tray
+        (.setImage (:dark resources))
+        (.setStatus "Wireguard: Disconnected"))
+
+      (let [menu (.getMenu tray)]
+        (add-menu-separator menu)
+        (doseq [item items]
+          (add-menu-item menu item))
+        (add-menu-separator menu)
+        (add-menu-item menu "Quit"
+                       (.shutdown tray))))))
+
+(defn read-wireguard-config-dir [& [dir]]
+  (-> dir
+      (or "/etc/wireguard")
+      io/file
+      file-seq
+      (->> (filter #(not (.isDirectory %)))
+           (map #(-> % .getName
+                     (->> (re-find #"(.+)\.conf")
+                          second)))
+           (into []))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (let [{:keys [options summary]} (parse-opts args cli-options)]
+    (cond
+      (:help options)
+      (println summary)
+
+      (:version options)
+      (println "Version:" version)
+
+      :else
+      (-> (read-wireguard-config-dir)
+          setup-switcher))))
