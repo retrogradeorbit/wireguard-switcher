796c0df51695c11ee47e5e93f1f9df46393db7be
Crispin Wellington <retrogradeorbit@gmail.com>
2019-09-21 23:55:46 +0800

    get menu connection/disconnection working

--- src/wireguard_switcher/core.clj
+++ src/wireguard_switcher/core.clj
@@ -1,6 +1,7 @@
 (ns wireguard-switcher.core
   (:require [clojure.java.io :as io]
             [clojure.string :as string]
+            [clojure.java.shell :as shell]
             [clojure.tools.cli :refer [parse-opts]])
   (:import [dorkbox.systemTray SystemTray MenuItem Separator Checkbox]
            [java.awt.event ActionListener])
@@ -38,20 +39,62 @@
 (defmacro add-menu-separator [menu]
   `(.add ~menu (Separator.)))
 
-(defn setup-switcher [items]
+(defn read-wireguard-state []
+  (let [{:keys [out err exit]} (shell/sh "sudo" "wg")]
+    (when (zero? exit)
+      (-> out
+          (string/split #"\n")
+          (->> (filter #(re-find #"^interface: " %))
+               (map #(->> %
+                          (re-find #"^interface: (.+)")
+                          second))
+               (into #{}))))))
+
+(defn change-wireguard-state [conn state]
+  (-> (shell/sh "sudo" "service" (str "wg-quick@" conn) (if state "start" "stop"))
+      :exit
+      zero?))
+
+(defn toggle-wireguard-state [conn]
+  (let [states (read-wireguard-state)
+        connected? (states conn)]
+    (if connected?
+      (change-wireguard-state conn false)
+      (change-wireguard-state conn true))))
+
+(defn set-check-marks-from-state [{:keys [tray menu-items]}]
+  (let [state (read-wireguard-state)
+        any-connections? (seq state)]
+    (doto tray
+      (.setImage (if any-connections? (:green resources) (:red resources)))
+      (.setStatus (str "Wireguard: "
+                       (if any-connections? "Connected" "Disconnected"))))
+
+    (doseq [[conn checkbox] menu-items]
+      (.setChecked checkbox (boolean (state conn))))))
+
+(defn setup-switcher [items connected]
   (when-let [tray (SystemTray/get)]
     (let [icon (:dark resources)]
       (doto tray
         (.setImage (:dark resources))
-        (.setStatus "Wireguard: Disconnected"))
+        (.setStatus (str "Wireguard: " (if (seq connected) "Connected" "Disconnected"))))
 
       (let [menu (.getMenu tray)]
         (add-menu-separator menu)
-        (doseq [item items]
-          (add-menu-item menu item))
-        (add-menu-separator menu)
-        (add-menu-item menu "Quit"
-                       (.shutdown tray))))))
+        (let [menu-items (into {}
+                               (for [item items]
+                                 (let [i (add-menu-checkbox
+                                          menu item
+                                          (toggle-wireguard-state item))]
+                                   (.setChecked i (boolean (connected item)))
+                                   [item i])))]
+          (add-menu-separator menu)
+          (add-menu-item menu "Quit"
+                         (.shutdown tray)
+                         (System/exit 0))
+          {:tray tray
+           :menu-items menu-items})))))
 
 (defn read-wireguard-config-dir [& [dir]]
   (-> dir
@@ -65,7 +108,7 @@
            (into []))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "main entry point for system tray menu"
   [& args]
   (let [{:keys [options summary]} (parse-opts args cli-options)]
     (cond
@@ -76,5 +119,10 @@
       (println "Version:" version)
 
       :else
-      (-> (read-wireguard-config-dir)
-          setup-switcher))))
+      (let [menu (setup-switcher
+                  (read-wireguard-config-dir)
+                  (read-wireguard-state))]
+        (loop []
+          (set-check-marks-from-state menu)
+          (Thread/sleep 1000)
+          (recur))))))
